b9f81cb7c768d14869e299dd7be1b8ae92b6087f
sal-araya <salemasheber@gmail.com>
2026-05-05 16:58:53 -0400

    Ex1B_variable_examples.py

---
+++ Week-05/Ex1B_variable_examples.py
@@ -0,0 +1,52 @@
+# Lab 1 - Variable Examples
+
+# Customer-related variables
+customer_id = 10245 # assuming this is a numeric ID, but could also be a string if it includes letters
+customer_name = "Nahom Tesfay" # using full name in one variable, but could be split into first_name and last_name
+customer_gender = "Male" # assuming text value, but could also use codes like 'M', 'F', or include more gender options
+customer_date_of_birth = "1995_08_14" # using string format YYYY_MM_DD, but could use a date type for better accuracy
+drivers_license_number = "D1234567" # assuming mix letters and numbers; format may vary by state
+auto_policy_number = "AP-98346" # assuming alphanumeric policy number, format depends on insurance company
+
+# Personal variables
+my_name = "Saliem Araya"  # full name in one variable, could also separate into first and last name
+city_state = "Sembel, Asmara"  # using a single variable for city and state, but could be split into city and state variables
+
+#Lab 2 
+
+# a) Full list of Python reserved words:
+
+# False, None, True,
+# and, as, assert, async, await,
+# break, class, continue,
+# def, del,
+# elif, else, except,
+#finally, for, from,
+# global,
+# if, import, in, is,
+# lambda,
+# nonlocal, not,
+# or,
+# pass,
+# raise, return,
+# try,
+# while, with,
+# yield
+
+
+# b) Keyword Definitions:
+
+# def → Used to define a function (a reusable block of code).
+# Example: def my_function():
+
+# if → Used for conditional statements (runs code only if condition is True).
+# Example: if x > 5:
+
+# for → Used to loop through a sequence (like a list or string).
+# Example: for i in range(5):
+
+# class → Used to define a ^^class^^, which is a blueprint for creating objects.
+# Example: class Student:
+
+# lambda → Creates a ^^anonymous function^^ (a function without a name).
+# Example: lambda x: x * 2
